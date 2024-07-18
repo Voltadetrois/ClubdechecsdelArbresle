c324f6e0530ad229d709c33624bef92a7fce557b
Voltadetrois <101822241+Voltadetrois@users.noreply.github.com>
2024-07-18 02:35:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #f0f0f0; /* Couleur de fond pour tout le corps */
+}
+
+header {
+    background-color: #333;
+    color: #fff;
+    padding: 1em 0;
+    text-align: center;
+}
+
+nav ul {
+    list-style: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin: 0 1em;
+}
+
+nav ul li a {
+    color: #fff;
+    text-decoration: none;
+}
+
+main {
+    padding: 2em;
+}
+
+section {
+    margin-bottom: 2em;
+    padding: 1em;
+    background-color: #fff; /* Couleur de fond pour chaque section */
+    border-radius:
+
